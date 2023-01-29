74a3dee75524575c7282e488c6e878dfc6994ffd
SYARGEY <lss1895@mail.ru>
2023-01-29 18:53:48 +0300

    Семинар 3 задание 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int InputInt (string massage)
+{
+    Console.WriteLine(massage);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int PolindromChek (int num)
+{
+    int rev =0;
+    while (num>0)
+    {
+        rev = rev*10+num%10;
+        num = num/10;
+    }
+    return rev;
+}
+int number = InputInt("Введите число");
+int reverse = PolindromChek(number);
+if (number == reverse)
+{
+    System.Console.WriteLine("Полиндром");
+}
+else
+{
+    System.Console.WriteLine("Не Полиндром");
+}
